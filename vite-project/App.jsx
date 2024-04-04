3cfc4ab53d8fd52b6fe889f3b5aa5fa7786719a1
559 <15735550843@163.com>
2024-04-04 10:26:03 +0800

    feat: 使用vite支持jsx

---
+++ vite-project/App.jsx
@@ -0,0 +1,12 @@
+import React from "./core/React.js";
+// const App = React.createElement("div", { id: "app" }, "hello ", "react");
+
+const App = <div>hello-mini-react</div>
+
+// function AppCopyr() {
+//     return <div>hello-mini-react</div>
+// }
+
+// console.log(AppCopyr);
+
+export default App;
